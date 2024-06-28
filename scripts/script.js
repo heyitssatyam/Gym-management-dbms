6d6fc3048fd5f995f52652ae7800fd4a630309da
heyitssatyam <kumarsatyam2003@gmail.com>
2024-06-28 13:22:22 +0530

    added dashboard

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.login-register');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const removeMain = document.querySelector('.main-body');
+const loginBtn = document.querySelector('.btn')
+
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -13,9 +16,16 @@ loginLink.addEventListener('click', ()=>{
 })
 
 btnPopup.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup')
+    removeMain.classList.add('hidden');
+    wrapper.classList.add('active-popup');
 })
 
 iconClose.addEventListener('click', ()=>{
-    wrapper.classList.remove('active-popup')
+    wrapper.classList.remove('active-popup');
+    removeMain.classList.remove("hidden");
+   
+})
+
+loginBtn.addEventListener('click', ()=>{
+    
 })
